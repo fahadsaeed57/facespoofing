e310c984dba13e9c233d8d7dae68c6ba1fed68f5
fahadsaeed57 <fahadsaeed57@gmail.com>
2019-03-13 18:55:22 +0500

    make it better

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template, request,jsonify
+from scipy.misc import imread, imresize
+import numpy as np
+import time
+app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/upload', methods=['POST','GET'])
+def upload_file():
+    file = request.files['image']
+    name = str(int(time.time()))
+    f = os.path.join(app.config['UPLOAD_FOLDER'],name)
+    file.save(f)
+    data = request.form['data']
+    return jsonify({"message":"uploaded img is " +str(name),"data":data})
+
+
+if __name__ == "__main__":
+    print(("* Starting server ..."))
+    app.run(debug=True)
